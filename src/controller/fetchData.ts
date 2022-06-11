2d37d9c410c5b2391d5f2a271e9f3c89c340f9b3
André Kuhlmann <akuhltime@gmail.com>
2022-06-11 23:23:43 +0200

    Enable caching
    Implemented a very simple in memory cache of the data that expires every 8 hours. This will safe up on request made to the actual website. This will also drastically improve performance.
    
    Max: 1095 request/year

--- src/controller/fetchData.ts
+++ src/controller/fetchData.ts
@@ -1,15 +1,29 @@
 import axios from 'axios'
 import parse from '../util/NLSTableParser'
 
+let refreshTimeHours = 8
+let lastUpdate: Date | undefined = undefined
+let dataSnapshot: any | undefined = undefined
+
 /**
  * Fetches the data from the website
  */
 async function fetchData(url: string) {
-	const response = await axios.get(url)
+	if (
+		lastUpdate == undefined ||
+		lastUpdate.getTime() >= new Date().getTime() + refreshTimeHours * 60 /*m*/ * 60 /*s*/ * 1000 /*ms*/
+	) {
+		console.log('chache not available || data older then ' + refreshTimeHours + ' hours, updating cache...')
+		const response = await axios.get(url)
+		const body = response.data
 
-	const body = response.data
+		dataSnapshot = parse(body)
+		lastUpdate = new Date()
+	} else {
+		console.log('chache available, using cache...')
+	}
 
-	return parse(body)
+	return dataSnapshot
 }
 
 export default fetchData
